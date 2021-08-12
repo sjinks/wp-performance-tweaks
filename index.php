9a61225e4530b50b2a84835c80663d7559ee395a
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2021-08-12 09:18:16 +0300

    Set version to 1.0.0

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin name: Performance tweaks
 Description: Performance tweaks for WordPress
 Author: Volodymyr Kolesnykov
 Author URI: https://wildwolf.name/
-Version: 1.0
+Version: 1.0.0
 */
 
 use WildWolf\WordPress\PerformanceTweaks\Attachments;
