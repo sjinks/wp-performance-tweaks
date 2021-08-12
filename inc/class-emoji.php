ef59183a9627ce3a88ba9c0c44104e4a50bc9d36
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2021-08-12 09:32:14 +0300

    More emoji tweaks

--- inc/class-emoji.php
+++ inc/class-emoji.php
@@ -17,6 +17,10 @@ final class Emoji {
 		remove_action( 'embed_head', 'print_emoji_detection_script' );
 		remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
+		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+
 		add_action( 'admin_init', [ $this, 'admin_init' ] );
 	}
 
