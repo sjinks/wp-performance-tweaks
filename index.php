0b8de44f8861049e1684f218a06a139a347a6d4c
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2022-06-30 22:48:13 +0300

    Add autoloader

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Description: Performance tweaks for WordPress
  * Author: Volodymyr Kolesnykov
  * Author URI: https://wildwolf.name/
- * Version: 1.1.5
+ * Version: 1.1.6
  */
 
 use WildWolf\WordPress\PerformanceTweaks\Plugin;
